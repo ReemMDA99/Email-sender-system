b2419406cfa5578e11595d6e11502b08a14828a1
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-18 15:48:38 -0600

    create testing com[ponents

--- startAngular/src/app/app.module.ts
+++ startAngular/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpService } from './Shared/http.service';
+import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PeofileChildComponent } from './peofile-child/peofile-child.component';
+import { CustomeDerectiveDirective } from './custome-derective.directive';
+import { StructuralDirectivesDirective } from './structural-directives.directive';
+import { AdditionPipe } from './pipes/addition.pipe';
+import { Material } from './app-material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    PageNotFoundComponent,
+    PeofileChildComponent,
+    CustomeDerectiveDirective,
+    StructuralDirectivesDirective,
+    AdditionPipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    Material,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
