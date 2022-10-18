c9b684de19b3822fe2b877852ad9518275d853f6
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-18 00:21:34 -0600

    create nodemailer functionality by creating reusable transporter object using the default SMTP transport

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,8 +1,8 @@
-// import/require dependencies express/cors/bodyparser/nodemailer
+// import/require dependencies express/cors/body-parser/nodemailer
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const nodemailer = require("nodemailer");
 
 // set up express routes to get and post request
 const app = express();
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 
 // add/send email
 app.post("/sendmail", (req, res) => {
-    console.log("request came");
+    console.log("Request received!");
     let user = req.body;
     sendMail(user, info => {
       console.log(`The mail has been sent with the id: ${info.messageId} 😃`);
@@ -32,3 +32,33 @@ app.post("/sendmail", (req, res) => {
     });
 });
 
+// set up nodemailer to create email sending functionality
+
+async function sendMail(user, callback) {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'reem.mda0909@gmail.com',
+        pass: 'ezblklrakneldrzd'
+      }
+    });
+  
+    let mailOptions = {
+      from: '"Email sent from Reem Ahmed"<reem.mda0909@gmail.com>', // sender address
+      to: 'india.canada96@yahoo.com', // list of receivers
+      subject: "This is a test email to confirm my app functionality 👻", // Subject line
+      html: `<h1>Hi ${user.name}</h1><br>
+      <h4>This is an Email Sender Application created by Reem, using technologies Node and Express for the Backend, 
+      Nodemailer for sending email, MySQL database and Angular for frontend </h4>`
+    };
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions);
+  
+    callback(info);
+}
+
+// main().catch(console.error);
